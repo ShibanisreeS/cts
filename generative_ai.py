a972501f66573910b7b2ca7a2587a69e1f92031e
ShibanisreeS <shib21ad059@rmkcet.ac.in>
2024-08-19 10:55:07 +0530

    Update generative_ai.py

--- generative_ai.py
+++ generative_ai.py
@@ -1,12 +1,13 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Retrieve the API key from environment variables
-api_key = os.getenv('GENAI_API_KEY')
+api_key = st.secrets['GENAI_API_KEY']
 
 def chat_bot(prompt):
     genai.configure(api_key=api_key)  # Replace with your actual API key
